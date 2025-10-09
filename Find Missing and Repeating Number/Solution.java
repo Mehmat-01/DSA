833d3faa6ac2b9ea88e13c0b8b14b197bad2a37e
Md Sami Ahmad <mdsamiahmad401@gmail.com>
2025-10-09 23:15:35 +0530

    Solution.java

--- Find Missing and Repeating Number/Solution.java
+++ Find Missing and Repeating Number/Solution.java
@@ -1,22 +1,22 @@
 import java.util.* ;
 import java.io.*; 
-import java.util.ArrayList;
+import java.util.ArrayList ;
 
-public class OptimalSolution {
+public class Solution {
 
     public static int[] missingAndRepeating(ArrayList<Integer> arr, int n) {
         
-        long s1=0;
-        long s2=0;
-        long s1n=(n*(n+1))/2;
+        long actual_sum=0;
+        long actual_sum_of_square=0;
+        long sn=(n*(n+1))/2;
         long s2n=(n*(n+1)*(2*n+1))/6;
         for(int i=0;i<n;i++){
-            s1+=arr.get(i);
-            s2+=(long)arr.get(i)*(long)arr.get(i);
+            actual_sum+=arr.get(i);
+            actual_sum_of_square+=(long)arr.get(i)*(long)arr.get(i);
 
         }
-        long val1=s1-s1n;
-        long val2=s2-s2n;
+        long val1=actual_sum-sn; //val1=x-y
+        long val2=actual_sum_of_square-s2n; //val2=x^2-y^2
         val2=val2/val1;
         long x=(val1+val2)/2;
         long y=x-val1;
@@ -25,3 +25,4 @@ public class OptimalSolution {
         
     }
 }
+
