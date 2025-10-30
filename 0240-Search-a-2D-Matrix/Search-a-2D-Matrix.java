184df48f29459048dc81d74a96c0be83e05a7a1a
Md Sami Ahmad <mdsamiahmad401@gmail.com>
2025-10-30 15:22:17 +0530

    Time: 6ms (46.91%), Space: 46.06 MB (60.96%) - Leetcode

---
+++ 0240-Search-a-2D-Matrix/Search-a-2D-Matrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=0;
+        int cols=matrix[0].length-1;
+        while(row<matrix.length && cols>=0){
+            if(matrix[row][cols]==target) return true;
+            else if(matrix[row][cols]>target) cols--;
+            else row++;
+        }
+        return false;
+    }
+}
