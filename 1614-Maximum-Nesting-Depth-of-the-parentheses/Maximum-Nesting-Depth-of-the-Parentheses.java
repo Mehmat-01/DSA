63a97130c4705435fee8bc34a020e547016a986d
Md Sami Ahmad <mdsamiahmad401@gmail.com>
2025-11-03 17:46:44 +0530

    Time: 0ms (100%), Space: 42.88 MB (9.55%) - Leetcode

---
+++ 1614-Maximum-Nesting-Depth-of-the-parentheses/Maximum-Nesting-Depth-of-the-Parentheses.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDepth(String s) {
+        int depth = 0, maxDepth = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                maxDepth = Math.max(maxDepth, ++depth);
+            else if (c == ')')
+                depth--;
+        }
+        return maxDepth;
+    }
+}
