9b6eff12bca6f6bf67795b04443e06e2f0012eaa
Md Sami Ahmad <mdsamiahmad401@gmail.com>
2025-11-05 18:48:52 +0530

    Time: 1ms (99.66%), Space: 45.63 MB (5.89%) - Leetcode

---
+++ 0443-String-Compression/String-Compression.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int compress(char[] chars) {
+        int write = 0;
+        int i = 0;
+        while (i < chars.length) {
+            int count = 0;
+            char current = chars[i];
+            while (i < chars.length && chars[i] == current) {
+                i++;
+                count++;
+            }
+            chars[write++] = current;
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+
+        }
+        return write;
+    }
+}
